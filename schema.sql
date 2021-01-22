fbd929ebe517c9991481541caaa6f606d14222c7
Jayashree Dattatraya Umale <jayashreeumale>
2021-01-22 12:06:37 +0530

    Database config

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE employees(id int AUTO_INCREMENT,firstName varchar(255),lastName varchar(255),emailId varchar(255),PRIMARY KEY(id));
+INSERT INTO employees VALUES(1,"jayashree","umale","j@gmail.com");
